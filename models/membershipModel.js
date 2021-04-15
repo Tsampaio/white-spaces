3395f6d136b7763ca04bcedfd13473f388ed21eb
Telmo Sampaio <telmoasampaio@gmail.com>
2021-04-15 22:36:16 +0100

    adding Membership admin model

---
+++ models/membershipModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const membershipSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+  },
+  userName: {
+    type: String,
+  },
+  userEmail: {
+    type: String,
+  },
+  customerId: {
+    type: String,
+  },
+  paidThrough: {
+      type: Date,
+  },
+  firstBillDate: {
+      type: Date,
+  },
+  status: {
+    type: String,
+  },
+  subscriptionId: {
+    type: String,
+  },
+  transactionId: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+});
+
+const Membership = mongoose.model('Membership', membershipSchema);
+
+module.exports = Membership;
